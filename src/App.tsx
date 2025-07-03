9ff0a61fa7139caf3447b3e70dc8140272d8e099
RUEL VELASQUEZ <ruelv232104@gmail.com>
2025-07-03 14:00:38 +0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import CropStatus from './pages/farmstatus/CropStatus'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [] = useState(0)
 
   return (
     <>
